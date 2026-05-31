b8c5ba68c731bcf318cb2a207d572c8ad6e439cc
alexei503 <54184321+alexei503@users.noreply.github.com>
2026-05-30 19:11:01 -0600

    exportación JSON y facturación electrónica
    modificación de exportación pdf para mostrar información de facturación electrónica
    implementación de botón de exportación Json

--- custom-addons/dte_sv/models/__init__.py
+++ custom-addons/dte_sv/models/__init__.py
@@ -1,3 +1,3 @@
-from . import res_company  # noqa: F401
-from . import res_partner  # noqa: F401
-from . import account_move  # noqa: F401
+from . import res_company
+from . import res_partner
+from . import account_move
